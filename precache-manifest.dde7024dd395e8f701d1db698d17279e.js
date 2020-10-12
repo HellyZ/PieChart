1eca505a31ebd3029720aacf40a5605cbd31f390
Alena Zhmurko <alenazhmurko@gmail.com>
2020-10-12 16:36:38 +0300

    Updates

---
+++ precache-manifest.dde7024dd395e8f701d1db698d17279e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12468d77fd2738360ed83208802304b4",
+    "url": "/PieChart/index.html"
+  },
+  {
+    "revision": "e5483f62c728abe5cdb3",
+    "url": "/PieChart/static/css/main.03ab09ca.chunk.css"
+  },
+  {
+    "revision": "aff06ffc68ec0c0a7e90",
+    "url": "/PieChart/static/js/2.b199cf48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PieChart/static/js/2.b199cf48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5483f62c728abe5cdb3",
+    "url": "/PieChart/static/js/main.af0cdf8f.chunk.js"
+  },
+  {
+    "revision": "f9c61e669e6c675d7a88",
+    "url": "/PieChart/static/js/runtime-main.3778d140.js"
+  }
+]);
